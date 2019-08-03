f718b09ff5fd2d8cb3af70bec2eaf8a0f590a703
unknown <kirill.krupina26@gmail.com>
2019-08-03 18:23:32 +0300

    add category & edit category in records page

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Category} from '../shared/models/category.model';
+import {CategoriesService} from '../shared/services/categories.service';
+
 @Component({
   selector: 'app-records-page',
   templateUrl: './records-page.component.html',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordsPageComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  isLoaded = false;
+
+  constructor(private categoryService: CategoriesService) { }
 
   ngOnInit() {
+    this.categoryService.getCategories()
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+        this.isLoaded = true;
+      });
+  }
+
+
+  newCategoryAdded(category: Category) {
+    this.categories.push(category);
+  }
+
+  categoryWasEdit(category: Category) {
+    const index = this.categories
+      .findIndex(c => c.id === category.id);
+    this.categories[index] = category;
+
   }
 
 }
