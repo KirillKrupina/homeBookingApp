b18cfed7e0999889a4e375024e89f949c0c1155e
unknown <kirill.krupina26@gmail.com>
2019-08-05 15:43:58 +0300

    added filter in history page & animation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {RouterModule} from '@angular/router';
+
+import {LoaderComponent} from './components/loader/loader.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule]
+  declarations: [LoaderComponent],
+  imports: [ReactiveFormsModule, FormsModule, NgxChartsModule, RouterModule],
+  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule, LoaderComponent],
 })
 
 export class SharedModule {
